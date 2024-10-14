e0d3adc06017ce8a94fe55eca5d047e0f439e14a
NightOwl <heroldmelfurze@gmail.com>
2024-10-14 13:34:56 +0800

    cmplate

--- plagscan2/backend/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Copyleaks\Copyleaks;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$API_KEY = 'f42d1909-1ad9-4403-b4e6-e6c9e37f4d1a';
-$API_SECRET = 'b2644caf-2e7c-4d70-a313-1c73449e1e8b';
-
